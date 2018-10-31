b13ff5d048265edd09007e44073b87628d8dde2d
Yuren Ju <yurenju@gmail.com>
2018-10-31 21:39:20 +0800

    fixed typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 
 $(npm bin)/set-up-ssh --key "$encrypted_4cd64f0ee7de_key" \
                         --iv "$encrypted_4cd64f0ee7de_iv" \
-                        --path-encrypted-key ".travis/github_deploy_key.enc
+                        --path-encrypted-key ".travis/github_deploy_key.enc"
 
 ./node_modules/.bin/gh-pages -d build/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
